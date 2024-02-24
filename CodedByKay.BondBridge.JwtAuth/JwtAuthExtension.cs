64149f4c9c415e4ce6994aa2197f0321f332cec7
Kay Wiberg <kaywib@gmail.com>
2024-02-24 14:36:19 +0100

    Adde comments

--- CodedByKay.BondBridge.JwtAuth/JwtAuthExtension.cs
+++ CodedByKay.BondBridge.JwtAuth/JwtAuthExtension.cs
@@ -8,6 +8,18 @@ namespace CodedByKay.BondBridge.JwtAuth
 {
     public static class JwtAuthExtension
     {
+        /// <summary>
+        /// Adds JWT-based authentication to the specified IServiceCollection.
+        /// </summary>
+        /// <param name="services">The IServiceCollection to add services to.</param>
+        /// <param name="secretKey">The secret key used for signing the JWT.</param>
+        /// <param name="issuer">The issuer of the JWT.</param>
+        /// <param name="audience">The audience of the JWT.</param>
+        /// <returns>The original IServiceCollection for chaining.</returns>
+        /// <remarks>
+        /// This method configures JWT authentication using the provided parameters. It sets up the authentication scheme, token validation parameters, 
+        /// and authorization policies for different user roles. Ensure the secretKey is securely stored and not hard-coded in production environments.
+        /// </remarks>
         public static IServiceCollection AddJwtAuthentication(this IServiceCollection services, string secretKey, string issuer, string audience)
         {
             var signingKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(secretKey));
@@ -36,7 +48,7 @@ namespace CodedByKay.BondBridge.JwtAuth
 
             services.AddAuthorization(options =>
             {
-                //Add more roles here to handel diffrent type of users: admin, user, editUser
+                // Add policies for different roles to handle various types of users such as admin, user, editUser
                 options.AddPolicy(
                     TokenValidationConstants.Policies.AuthAPIAdmin,
                     policy => policy.RequireClaim(
